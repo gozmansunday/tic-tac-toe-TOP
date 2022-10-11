3f75d0415678facbcc639f54d4f0b144bc1eb4c6
Gozman <thegzmn@gmail.com>
2022-10-12 00:09:55 +0100

    Update logic

--- script.js
+++ script.js
@@ -43,8 +43,6 @@ const GameBoard = (() => {
       gameInfo.textContent = `${playerOName} TO START THE GAME!`
       gameInfo.classList.remove('invisible');
     }
-
-    console.log(gameBoardArray); //! REMOVE LATER
   };
 
   return {gameBoard, gameBoardArray, display, gameInfo, playAgainBtn};
@@ -78,14 +76,14 @@ const Player = (() => {
     GameBoard.display();
   });
 
-  return {playerX, playerO};
+  return {playerXDisp, playerODisp, playerX, playerO};
 })();
 
 // Module to control flow of game
 const GameFlow = (() => {
-  const gameInfo = document.querySelector('#game-info');
-  const playAgainBtn = document.querySelector('#play-again-btn');
   const gameBoard = GameBoard.gameBoard;
+  const gameInfo = GameBoard.gameInfo;
+  const playAgainBtn = GameBoard.playAgainBtn;
   let gameBoardArray = GameBoard.gameBoardArray;
   const noOfBoxes = gameBoardArray.length;
   let playerX = Player.playerX;
@@ -113,8 +111,8 @@ const GameFlow = (() => {
   // Function to place X or O in the box when it is clicked
   const showMark = () => {
     gameBoard.addEventListener('click', (e) => {
-      const playerXDisp = document.querySelector('#player-x-disp');
-      const playerODisp = document.querySelector('#player-o-disp');
+      const playerXDisp = Player.playerXDisp;
+      const playerODisp = Player.playerODisp;
       const playerXName = playerXDisp.textContent;
       const playerOName = playerODisp.textContent;
       let box = e.target;
@@ -154,20 +152,17 @@ const GameFlow = (() => {
     } else if (gameBoardArray[boxIndex].mark === 'O') {
       coordinatesO.push(+boxIndex);
     }
-    console.log(coordinatesX);
-    console.log(coordinatesO);
     checkWin();
   }
 
   // Function to check if a player has won the game
   function checkWin() {
-    const playerXDisp = document.querySelector('#player-x-disp');
-    const playerODisp = document.querySelector('#player-o-disp');
+    const playerXDisp = Player.playerXDisp;
+    const playerODisp = Player.playerODisp;
     const playerXName = playerXDisp.textContent;
     const playerOName = playerODisp.textContent;
 
     if ((coordinatesX.includes(0) && coordinatesX.includes(1) && coordinatesX.includes(2)) || (coordinatesX.includes(3) && coordinatesX.includes(4) && coordinatesX.includes(5)) || (coordinatesX.includes(6) && coordinatesX.includes(7) && coordinatesX.includes(8)) || (coordinatesX.includes(0) && coordinatesX.includes(3) && coordinatesX.includes(6)) || (coordinatesX.includes(1) && coordinatesX.includes(4) && coordinatesX.includes(7)) || (coordinatesX.includes(2) && coordinatesX.includes(5) && coordinatesX.includes(8)) || (coordinatesX.includes(0) && coordinatesX.includes(4) && coordinatesX.includes(8)) || (coordinatesX.includes(2) && coordinatesX.includes(4) && coordinatesX.includes(6))) {
-      console.log(`${playerXName} WINS!`);
       gameIsOver = true;
       gameInfo.textContent = `${playerXName} WINS!`;
       GameBoard.gameInfo.classList.remove('invisible');
@@ -175,7 +170,6 @@ const GameFlow = (() => {
       playerO.toPlay = true;
       playerX.toPlay = false;
     } else if (((coordinatesO.includes(0) && coordinatesO.includes(1) && coordinatesO.includes(2)) || (coordinatesO.includes(3) && coordinatesO.includes(4) && coordinatesO.includes(5)) || (coordinatesO.includes(6) && coordinatesO.includes(7) && coordinatesO.includes(8)) || (coordinatesO.includes(0) && coordinatesO.includes(3) && coordinatesO.includes(6)) || (coordinatesO.includes(1) && coordinatesO.includes(4) && coordinatesO.includes(7)) || (coordinatesO.includes(2) && coordinatesO.includes(5) && coordinatesO.includes(8)) || (coordinatesO.includes(0) && coordinatesO.includes(4) && coordinatesO.includes(8)) || (coordinatesO.includes(2) && coordinatesO.includes(4) && coordinatesO.includes(6)))) {
-      console.log(`${playerOName} WINS!`);
       gameIsOver = true;
       gameInfo.textContent = `${playerOName} WINS!`;
       GameBoard.gameInfo.classList.remove('invisible');
@@ -183,7 +177,6 @@ const GameFlow = (() => {
       playerX.toPlay = true;
       playerO.toPlay = false;
     } else if (gameBoardArray[0].clicked === true && gameBoardArray[1].clicked === true && gameBoardArray[2].clicked === true && gameBoardArray[3].clicked === true && gameBoardArray[4].clicked === true && gameBoardArray[5].clicked === true && gameBoardArray[6].clicked === true && gameBoardArray[7].clicked === true && gameBoardArray[8].clicked === true) {
-      console.log(`IT'S A TIE!`);
       gameIsOver = true;
       gameInfo.textContent = `IT'S A TIE!`;
       GameBoard.gameInfo.classList.remove('invisible');
